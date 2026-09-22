3cbc26dc0e1bcf0ed689f68c9024210242e1dbff
由风 <youfeng11@outlook.com>
2026-09-22 23:53:24 +0000

    chore: 修改版本为0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.youfeng.plugin.vulkanfix"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         ndk {
             abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86"))
